1acbd2f8fd85be84a9a5e9f5268bc47628d4cdba
SWHL <liekkaskono@163.com>
2026-09-18 22:58:11 +0800

    chore: remove ai related files

--- packages/ai/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './types';
-export * from './prompts';
-export * from './humanize-rules';
-export * from './segmentation';
-export * from './openai-compatible';
-export * from './rewrite';
